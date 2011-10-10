37d84a9035d81eda02a1eb77cb97339e21f6c89b
msvanbee <msvanbee@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-10-10 07:36:46 +0000

    added 2 constructors, one for single class, one for full option

--- src/edu/calpoly/razsoftware/CourseOption.java
+++ src/edu/calpoly/razsoftware/CourseOption.java
@@ -1,12 +1,28 @@
 package edu.calpoly.razsoftware;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 public class CourseOption
 {
     private String requirement;
     private Set<Course> options;
     private boolean mutuallyExclusive;
+    
+    public CourseOption(Course onlyCourse)
+    {
+        this.requirement = onlyCourse.toString();
+        this.options = new TreeSet<Course>();
+        this.options.add(onlyCourse);
+        this.mutuallyExclusive = true;
+    }
+    
+    public CourseOption(String name, Set<Course> options, boolean mutuallyExclusive)
+    {
+        this.requirement = name;
+        this.options = options;
+        this.mutuallyExclusive = mutuallyExclusive;
+    }
 
     public boolean isMutuallyExclusive()
     {
