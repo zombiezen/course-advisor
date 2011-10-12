ef354c72f0a6666338fd06f44b6e82ef5f328d55
rlight <rlight@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-10-12 00:27:06 +0000

    Add partial unit tests for Course
    Notably, emptyPrerequisitesShouldAlwaysMatch fails right now.  Assigned to
    Michael.

---
+++ test/edu/calpoly/razsoftware/CourseTest.java
@@ -0,0 +1,80 @@
+package edu.calpoly.razsoftware;
+
+import java.util.Set;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CourseTest
+{
+    @Test
+    public void constructorTest()
+    {
+        Course c = new Course(ImmutableList.of("CSC", "CPE"), 101, 4,
+                              "Hello, World!");
+        assertEquals(ImmutableList.of("CSC", "CPE"), c.getMajor());
+        assertEquals(101, c.getNumber());
+        // TODO(rlight): name isn't being constructed. :(
+        assertEquals(4, c.getUnits());
+        assertEquals("Hello, World!", c.getDescription());
+        assertEquals(ImmutableSet.<Set<Course>>of(), c.getPreRequisites());
+        assertEquals(ImmutableSet.<Set<Course>>of(), c.getCoRequisites());
+    }
+
+    @Test
+    public void emptyPrerequisitesShouldAlwaysMatch()
+    {
+        final Course c1 = new Course(ImmutableList.of("CSC"), 101, 4, "101");
+        final Course c2 = new Course(ImmutableList.of("CSC"), 102, 4, "102");
+
+        assertTrue(c1.preRecsMet(ImmutableSet.<Course>of()));
+        assertTrue(c1.preRecsMet(ImmutableSet.<Course>of(c2)));
+    }
+
+    @Test
+    public void emptyShouldNotMatch()
+    {
+        final Course c1 = new Course(ImmutableList.of("CSC"), 101, 4, "101");
+        final Course c2 = new Course(ImmutableList.of("CSC"), 102, 4, "102");
+
+        c2.getPreRequisites().add(ImmutableSet.<Course>of(c1));
+
+        assertFalse(c2.preRecsMet(ImmutableSet.<Course>of()));
+    }
+
+    @Test
+    public void subsetShouldNotMatch()
+    {
+        final Course c1 = new Course(ImmutableList.of("CSC"), 141, 4, "141");
+        final Course c2 = new Course(ImmutableList.of("CSC"), 102, 4, "102");
+        final Course c3 = new Course(ImmutableList.of("CSC"), 103, 4, "103");
+
+        c3.getPreRequisites().add(ImmutableSet.<Course>of(c1, c2));
+
+        assertFalse(c3.preRecsMet(ImmutableSet.<Course>of(c1)));
+    }
+
+    @Test
+    public void exactShouldMatch()
+    {
+        final Course c1 = new Course(ImmutableList.of("CSC"), 101, 4, "101");
+        final Course c2 = new Course(ImmutableList.of("CSC"), 102, 4, "102");
+
+        c2.getPreRequisites().add(ImmutableSet.<Course>of(c1));
+
+        assertTrue(c2.preRecsMet(ImmutableSet.<Course>of(c1)));
+    }
+
+    @Test
+    public void supersetShouldMatch()
+    {
+        final Course c1 = new Course(ImmutableList.of("CSC"), 101, 4, "101");
+        final Course c2 = new Course(ImmutableList.of("CSC"), 102, 4, "102");
+        final Course c3 = new Course(ImmutableList.of("CSC"), 141, 4, "141");
+
+        c2.getPreRequisites().add(ImmutableSet.<Course>of(c1));
+
+        assertTrue(c2.preRecsMet(ImmutableSet.<Course>of(c1, c3)));
+    }
+}
