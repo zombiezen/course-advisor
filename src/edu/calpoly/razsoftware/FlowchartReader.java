00eb567bff17a8bbdc6dd179a813840fd7c83091
aspurgin <aspurgin@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-10-17 16:25:56 +0000

    courselist integration, inputstream capability

--- src/edu/calpoly/razsoftware/FlowchartReader.java
+++ src/edu/calpoly/razsoftware/FlowchartReader.java
@@ -3,33 +3,67 @@ package edu.calpoly.razsoftware;
 import java.io.File;
 import com.google.gson.*;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FlowchartReader
 {
+    private static Gson gson = new Gson();
     /**
      * provides a flowchart based off a JSON file containing newline-delineated "CourseOption" classes
      * @param path location of the JSON file
      * @return a flowchart based off the file
      */
-    public static Flowchart readFlowchart(File path)
+    public static Flowchart readFlowchart(File path, CourseList courses)
     {
-        Scanner s;
-        Gson gson = new Gson();
-        Flowchart fc = new Flowchart();
+        HashSet<CourseOption> options = new HashSet<CourseOption>();
+        Scanner s = null;
         try
         {
             s = new Scanner(path);
-            while(s.hasNext())
-            {
-                fc.addOption(gson.fromJson(s.nextLine(), CourseOption.class));
-            }
         }
-        catch(FileNotFoundException e)
+        catch (FileNotFoundException ex)
         {
-            e.printStackTrace();
+            Logger.getLogger(FlowchartReader.       class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return fc;
+        HashSet<Course> tmpCourseList = new HashSet<Course>();
+        while(s.hasNextLine())
+        {
+            CourseOption co = gson.fromJson(s.nextLine(), CourseOption.class);
+            for(Course c : co.getOptions())
+            {
+                Course course = courses.lookUp(c.getMajor().get(0), c.getNumber());
+                if(course != null)
+                {
+                    c = course;
+                }
+            }
+            options.add(co);
+        }
+        return new Flowchart(options);
+    }
+
+    public static Flowchart readFlowchart(InputStream iStream, CourseList courses)
+    {
+        HashSet<CourseOption> options = new HashSet<CourseOption>();
+        Scanner s = new Scanner(iStream);
+        HashSet<Course> tmpCourseList = new HashSet<Course>();
+        while(s.hasNextLine())
+        {
+            CourseOption co = gson.fromJson(s.nextLine(), CourseOption.class);
+            for(Course c : co.getOptions())
+            {
+                Course course = courses.lookUp(c.getMajor().get(0), c.getNumber());
+                if(course != null)
+                {
+                    c = course;
+                }
+            }
+            options.add(co);
+        }
+        return new Flowchart(options);
     }
 }
