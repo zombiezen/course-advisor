fda9ec3f1bcbeb995d22fec5fc6e1e5127a51453
Ross Light <ross@zombiezen.com>
2011-10-17 16:39:37 +0000

    Change CourseDecider to match Ticket #39

--- src/edu/calpoly/razsoftware/CourseDecider.java
+++ src/edu/calpoly/razsoftware/CourseDecider.java
@@ -7,58 +7,22 @@ import java.util.Set;
 
 public class CourseDecider
 {
-    public static class Result
-    {
-        private Set<Course> preReqMet;
-        private Set<Course> preReqNotMet;
-
-        public Result(final Set<Course> preReqMet,
-                      final Set<Course> preReqNotMet)
-        {
-            this.preReqMet = preReqMet;
-            this.preReqNotMet = preReqNotMet;
-        }
-
-        public Set<Course> getPrerequisitesMet()
-        {
-            return preReqMet;
-        }
-
-        public Set<Course> getPrerequisitesNotMet()
-        {
-            return preReqNotMet;
-        }
-    }
-
-    public Result decideClasses(UserState state, Flowchart flowchart)
+    public Set<CourseOption> decideClasses(UserState state, Flowchart flowchart)
     {
         final Set<CourseOption> sectionReqs = flowchart.getSectionReqs();
         final ImmutableSet<Course> taken =
                 ImmutableSet.copyOf(state.getTaken());
-        final Set<Course> need = new HashSet<Course>();
-        final Set<Course> met = new HashSet<Course>();
-        final Set<Course> notMet = new HashSet<Course>();
+        final Set<CourseOption> notFulfilled = new HashSet<CourseOption>();
 
         for (CourseOption req : sectionReqs)
         {
             final Set<Course> options = ImmutableSet.copyOf(req.getOptions());
             if (Sets.intersection(taken, options).isEmpty())
             {
-                // Requirement not fulfilled
-                for (Course neededCourse : options)
-                {
-                    if (neededCourse.preRecsMet(taken))
-                    {
-                        met.add(neededCourse);
-                    }
-                    else
-                    {
-                        notMet.add(neededCourse);
-                    }
-                }
+                notFulfilled.add(req);
             }
         }
 
-        return new Result(met, notMet);
+        return notFulfilled;
     }
 }
