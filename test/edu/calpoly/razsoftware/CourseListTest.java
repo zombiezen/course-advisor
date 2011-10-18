cd613fada45906d66f8d87c04c57c42fd74dfefa
Ross Light <ross@zombiezen.com>
2011-10-18 19:27:22 +0000

    Add another assertion to CourseListTest (this should really verify everything)

--- test/edu/calpoly/razsoftware/CourseListTest.java
+++ test/edu/calpoly/razsoftware/CourseListTest.java
@@ -4,6 +4,7 @@
  */
 package edu.calpoly.razsoftware;
 
+import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -22,10 +23,10 @@ public class CourseListTest
         final Course cpe102 = list.lookUp("CPE", 102);
         final Course cpe103 = list.lookUp("CPE", 103);
         final Course csc141 = list.lookUp("CSC", 141);
-        
+
         assertEquals(1, cpe103.getPreRequisites().size());
         assertEquals(2, cpe103.getPreRequisites().iterator().next().size());
-        
+
         for (Course c : cpe103.getPreRequisites().iterator().next())
         {
             if (c.getMajor().get(0).equals("CPE") && c.getNumber() == 102)
@@ -37,5 +38,8 @@ public class CourseListTest
                 assertSame(csc141, c);
             }
         }
+
+        assertEquals(ImmutableSet.of(ImmutableSet.of(cpe102, csc141)),
+                     cpe103.getPreRequisites());
     }
 }
