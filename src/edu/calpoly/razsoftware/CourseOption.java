712920d847382f751a3e3cf0ee8c286ca124ae39
djohns34 <djohns34@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-10-19 03:26:52 +0000

    Added a compareTo method to CourseOption so they can be sorted by suggested quarter. Implemented the Suggest Schedule button in the frame

--- src/edu/calpoly/razsoftware/CourseOption.java
+++ src/edu/calpoly/razsoftware/CourseOption.java
@@ -3,13 +3,13 @@ package edu.calpoly.razsoftware;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class CourseOption
+public class CourseOption implements Comparable<CourseOption>
 {
-    private String requirement;
+    private String      requirement;
     private Set<Course> options;
-    private boolean mutuallyExclusive;
-    private int quarter;
-    
+    private boolean     mutuallyExclusive;
+    private int         quarter;
+
     public CourseOption(Course onlyCourse, int quarterToTake)
     {
         this.requirement = onlyCourse.toString();
@@ -18,8 +18,9 @@ public class CourseOption
         this.mutuallyExclusive = true;
         this.quarter = quarterToTake;
     }
-    
-    public CourseOption(String name, Set<Course> options, boolean mutuallyExclusive, int quarterToTake)
+
+    public CourseOption(String name, Set<Course> options,
+            boolean mutuallyExclusive, int quarterToTake)
     {
         this.requirement = name;
         this.options = options;
@@ -41,25 +42,40 @@ public class CourseOption
     {
         return requirement;
     }
-    
+
     public int getQuarter()
     {
         return quarter;
     }
-    
+
     @Override
     public String toString()
     {
         String req = "";
-        
-        if(options.size() > 0)
+
+        if (options.size() > 0)
         {
-            for(Course c : options)
+            for (Course c : options)
                 req += c.toString() + " or ";
-            
-            req = req.substring(0,req.length() - 4);
+
+            req = req.substring(0, req.length() - 4);
         }
-        
+
         return req;
+    }
+
+    @Override
+    public int compareTo(CourseOption o)
+    {
+        if (quarter == o.quarter)
+        {
+            return 0;
+        }
+        else if (quarter < o.quarter)
+        {
+            return -1;
+        }
+        return 1;
+        // return 0;
     }
 }
