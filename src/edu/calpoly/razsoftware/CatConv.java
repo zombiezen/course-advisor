6cd63f9579f79773656c61746bef776da5029fea
aspurgin <aspurgin@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-11-17 23:35:34 +0000

    Pseudocode

--- src/edu/calpoly/razsoftware/CatConv.java
+++ src/edu/calpoly/razsoftware/CatConv.java
@@ -26,185 +26,212 @@ import java.util.List;
 
 public class CatConv 
 {
-	  /*  private class Course
-		{
-    public List<String> major;
-    public int number, units;
-    public String description;
-    public String name = "";
-    public Set<Set<Course>> preRequisites;//list of lists for possiblities, outside or, inside and
-    public Set<Set<Course>> coRequisites;// pre or co ^^
+
+public static void main(String[] args) throws IOException
+{
     
-    public Course()
+    InputStream inputStream = CatConv.class.getResourceAsStream("Cat.txt");
+    //INITIALIZE file to resource destination
+    File file = new File("/Users/michaelsvanbeek/Downloads/cat.txt");
+    //INITIALIZE gson object
+    Gson gson = new Gson();
+    //INITIALIZE scanner object
+    Scanner scanner = new Scanner(file);
+    //INITIALIZE scanner object
+    ArrayList<CatConvCourse> CatConvCourses = new ArrayList<CatConvCourse>();
+
+    //WHILE there are still lines left to be scanned
+    while(scanner.hasNextLine())
     {
-    	major = new ArrayList<String>();
-    	number = 0;
-    	units = 0;
-    	description = new String("");
-    	preRequisites = new HashSet<Set<Course>>();
-    	coRequisites = new HashSet<Set<Course>>();
+        //SET currentline to the next line
+        String currentLine =scanner.nextLine();
+        //IF the current line matches the pattern signifying a new entry THEN
+        if(currentLine.matches("[A-Za-z]{3,4} [0-9]{3} [^(]* .*[(].*"))
+        {
+            //INITIALIZE a new course
+            CatConvCourse newCatConvCourse = new CatConvCourse();
+            //SET index to 2
+            int tokenIndex=2;
+            //SET spl to be an array of tokens provided by splitting the current
+            //line at any space character
+            String[] spl = currentLine.split(" ");
+            //SET the major of the course to the first item in the list
+            newCatConvCourse.major.add(spl[0]);
+            //SET the number of the course to the second item in the list
+            newCatConvCourse.number = Integer.valueOf(spl[1]);
+            try
+            {
+                //FOR each token that does not begin with "("
+                for(; !spl[tokenIndex].startsWith("(");tokenIndex++)
+                {
+                    //add token to the name of the course
+                    newCatConvCourse.name += (spl[tokenIndex]+" ");
+                }
+                //SET the units of the course to the value in the parenthases
+                newCatConvCourse.units = Integer.valueOf(spl[tokenIndex].charAt(1)-'0');
+            }
+            catch(Exception E)
+            {
+                System.out.println(currentLine);
+            }
+            //IF the current line indicates the class has more listings THEN
+            if(currentLine.contains("(Also listed as"))
+            {
+                //add the alternate listings to the major list
+                String[] altListing = currentLine.split("\\(Also listed as", 2);
+                newCatConvCourse.major.add(altListing[1].split(" ")[1]);
+            }
+            //add the current course to the list
+            CatConvCourses.add(newCatConvCourse);
+        }
+        //ELSE IF the list of courses is not empty
+        else if(CatConvCourses.size()>0)
+        {
+            //set current course to the course on top of the list
+            CatConvCourse course = CatConvCourses.remove(CatConvCourses.size()-1);
+            //append description to the course
+            course.description += currentLine;
+            //put it back
+            CatConvCourses.add(course);
+        }
     }
+    //INITIALIZE jsonout
+    File jsonout = new File("NewCat.json");
+    //INITIALISE a filewriter to jsonout
+    FileWriter fwjson = new FileWriter(jsonout);
+    //FOR each course in the list
+    for(CatConvCourse currentCourse : CatConvCourses)
+    {
+        //IF the current course description starts with "(Also listed as" THEN
+        if(currentCourse.description.startsWith("(Also listed as"))
+        {
+            //split the string after the closing parenthases
+            String[] spl = currentCourse.description.split("\\)", 2);
+            currentCourse.description = spl[1];
+            //set the description to the last half of the split
+            String[] spl2 = spl[0].split(" ");
+            //add the alternative major listing tot eh course
+            currentCourse.major.add(spl2[3]);
+        }
+        //ENDIF
+        //IF the description contains the word "Prerequisite:" THEN
+        if(currentCourse.description.contains("Prerequisite:"))
+        {
+            //split the description at "Prerequisite:"
+            String[] tokens = currentCourse.description.split("Prerequisite:");
+            //set the description to the first half
+            currentCourse.description = tokens[0];
+            //IF the second half contains "Corequisite" THEN
+            if(tokens[1].contains("Corequisite"))
+            {
+                //split the description at "Corequisite"
+                String[] tokenTokens = tokens[1].split("Corequisite:");
+                //parse first half of second part as prerequisites
+                currentCourse.preRequisites = parseReq(tokenTokens[0]);
+                //parse second half as of second part corequisites
+                currentCourse.coRequisites = parseReq(tokenTokens[1]);
+            }
+            //ELSE
+            else
+            {
+                //parse second half as prerequisites
+                currentCourse.preRequisites = parseReq(tokens[1]);
+            }
+        }
+        //ENDIF
+        //IF
+        if(currentCourse.description.contains("prerequisite:"))
+        {
+            String[] tokens = currentCourse.description.split("prerequisite:");
+            currentCourse.description = tokens[0];
+            if(tokens[1].contains("corequisite"))
+            {
+                String[] spl2 = tokens[1].split("corequisite:");
+                currentCourse.preRequisites = parseReq(spl2[0]);
+                currentCourse.coRequisites = parseReq(spl2[1]);
+            }
+            else
+            {
+                currentCourse.preRequisites = parseReq(tokens[1]);
+            }
+        }
+        //export the course to the file
+        fwjson.append(gson.toJson(currentCourse) + "\n");
+    }
+    //close the writer
+    fwjson.close();
+        
+}
     
-    public Course(String major, int number)
+    public static Set<Set<CatConvCourse>> parseReq(String in)
     {
-    	this.major = new ArrayList<String>();
-    	this.major.add(major);
-    	this.number = number;
-    }
-		}*/
-	public static void main(String[] args) throws IOException
-	{
-                InputStream is = CatConv.class.getResourceAsStream("Cat.txt");
-                File f = new File("/Users/michaelsvanbeek/Downloads/cat.txt");
-		Gson gson = new Gson();
-		Scanner s = new Scanner(f);
-		ArrayList<CatConvCourse> CatConvCourses = new ArrayList<CatConvCourse>();
-		while(s.hasNextLine())
-		{
-    String tmp =s.nextLine();
-    if(tmp.matches("[A-Za-z]{3,4} [0-9]{3} [^(]* .*[(].*"))
-    {
-    	CatConvCourse newCatConvCourse = new CatConvCourse();
-    	int i=2;
-    	String[] spl = tmp.split(" ");
-    	newCatConvCourse.major.add(spl[0]);
-    	newCatConvCourse.number = Integer.valueOf(spl[1]);
-    	try
-    	{
-    		for(; !spl[i].startsWith("(");i++)
-    		{
-        newCatConvCourse.name += (spl[i]+" ");
-    		}
-    		newCatConvCourse.units = Integer.valueOf(spl[i].charAt(1)-'0');
-    	}
-    	catch(Exception E)
-    	{
-    		System.out.println(tmp);
-    	}
-    	
-    	
-    	
-    	if(tmp.contains("(Also listed as"))
-    	{
-    		String[] alt = tmp.split("\\(Also listed as", 2);
-    		newCatConvCourse.major.add(alt[1].split(" ")[1]);
-    	}
-    	
-    	CatConvCourses.add(newCatConvCourse);
-    }
-    else if(CatConvCourses.size()>0)
-    {
-    	CatConvCourse c = CatConvCourses.remove(CatConvCourses.size()-1);
-    	c.description += tmp;
-    	CatConvCourses.add(c);
-    }
-		}
-		File jsonout = new File("NewCat.json");
-		FileWriter fwjson = new FileWriter(jsonout);
-		for(CatConvCourse c : CatConvCourses)
-		{
-    if(c.description.startsWith("(Also listed as"))
-    {
-    	String[] spl = c.description.split("\\)", 2);
-    	c.description = spl[1];
-    	String[] spl2 = spl[0].split(" ");
-    	c.major.add(spl2[3]);
-    }
-    
-    
-    if(c.description.contains("Prerequisite:"))
-    {
-    	String[] spl = c.description.split("Prerequisite:");
-    	c.description = spl[0];
-    	if(spl[1].contains("Corequisite"))
-    	{
-    		String[] spl2 = spl[1].split("Corequisite:");
-    		c.preRequisites = parseReq(spl2[0]);
-    		c.coRequisites = parseReq(spl2[1]);
-    	}
-    	else
-    	{
-    		c.preRequisites = parseReq(spl[1]);
-    	}
-    }
-    
-    
-    if(c.description.contains("prerequisite:"))
-    {
-    	String[] spl = c.description.split("prerequisite:");
-    	c.description = spl[0];
-    	if(spl[1].contains("corequisite"))
-    	{
-    		String[] spl2 = spl[1].split("corequisite:");
-    		c.preRequisites = parseReq(spl2[0]);
-    		c.coRequisites = parseReq(spl2[1]);
-    	}
-    	else
-    	{
-    		c.preRequisites = parseReq(spl[1]);
-    	}
-    }
-    fwjson.append(gson.toJson(c) + "\n");
-		}
-		fwjson.close();
-		
-	}
-	
-	public static Set<Set<CatConvCourse>> parseReq(String in)
-	{
-		Pattern CatConvCoursePattern = Pattern.compile("[A-Z]{3,4} [0-9]{3}");
-		HashSet<Set<CatConvCourse>> req = new HashSet<Set<CatConvCourse>>();
-		String[] stage1 = in.split("and|,|;");
-		
-		
-		for(String s : stage1)
-		{
-    HashSet<CatConvCourse> out = new HashSet<CatConvCourse>();
-    Matcher m = CatConvCoursePattern.matcher(s);
-    while(m.find())
-    {
-    	out.add(new CatConvCourse(m.group().split(" ")[0], Integer.valueOf(m.group().split(" ")[1])));
-    }
-    if(out.size() != 0)
-    	req.add(out);
-		}
-		if(req.size()==0)
-                    return null;
-		else
-		{
-                    HashSet<Set<CatConvCourse>> permutations = new HashSet<Set<CatConvCourse>>();
-                    for(Set<CatConvCourse> s : req)
+        //initialize the pattern to check
+        Pattern CatConvCoursePattern = Pattern.compile("[A-Z]{3,4} [0-9]{3}");
+        //INITIALIZE the hashset to rebuild
+        HashSet<Set<CatConvCourse>> req = new HashSet<Set<CatConvCourse>>();
+        //split the plain english statement up by common deliniators
+        String[] stage1 = in.split("and|,|;");
+
+        //FOR every string that was just generated
+        for(String s : stage1)
+        {
+            //INITIALIZE a new set of courses
+            HashSet<CatConvCourse> out = new HashSet<CatConvCourse>();
+            
+            //FOR every pattern that matches a class, create a new class and add
+            //it to the list
+            Matcher m = CatConvCoursePattern.matcher(s);
+            while(m.find())
+            {
+                out.add(new CatConvCourse(m.group().split(" ")[0], Integer.valueOf(m.group().split(" ")[1])));
+            }
+            //ENDFOR
+            
+            //IF a class wa added
+            if(out.size() != 0)
+                //Add the requisite to the hashset
+                req.add(out);
+        }
+        //ENDFOR
+        //IF the requisite list is empty THEN
+        if(req.size()==0)
+            return null;
+        //ELSE
+        else
+        {
+            HashSet<Set<CatConvCourse>> permutations = new HashSet<Set<CatConvCourse>>();
+            for(Set<CatConvCourse> s : req)
+            {
+                if(permutations.isEmpty())
+                {
+                    for(CatConvCourse c : s)
                     {
-                        if(permutations.isEmpty())
-                        {
-                                for(CatConvCourse c : s)
-                                {
                         HashSet<CatConvCourse> newitem = new HashSet<CatConvCourse>();
                         newitem.add(c);
                         permutations.add(newitem);
-                                }
-                        }
-                        else
-                        {
-                                HashSet<Set<CatConvCourse>> newPerm = new HashSet<Set<CatConvCourse>>();
-                                for(Set<CatConvCourse> s2 : permutations)
-                                {
+                    }
+                }
+                else
+                {
+                    HashSet<Set<CatConvCourse>> newPerm = new HashSet<Set<CatConvCourse>>();
+                    for(Set<CatConvCourse> s2 : permutations)
+                    {
                         for(CatConvCourse c : s)
                         {
-                                HashSet<CatConvCourse> dupeset = new HashSet<CatConvCourse>(s2);
-                                dupeset.add(c);
-                                newPerm.add(dupeset);
+                            HashSet<CatConvCourse> dupeset = new HashSet<CatConvCourse>(s2);
+                            dupeset.add(c);
+                            newPerm.add(dupeset);
                         }
-                                }
-                                permutations = new HashSet<Set<CatConvCourse>>(newPerm);
-
-                        }
-
                     }
-                    req = permutations;
+                    permutations = new HashSet<Set<CatConvCourse>>(newPerm);
+
                 }
-                return req;
-	}
-	
+
+            }
+            req = permutations;
+        }
+        return req;
+    }
+    
 }
 
