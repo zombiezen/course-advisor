bff143b8ecc5b9d31ff60de2ce0dc39523f5b3ef
djohns34 <djohns34@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-11-17 06:08:34 +0000

    Reworked the GUI so the netbeans gui builder would parse the file Verified comments and ckeckstyle in  classes

--- src/edu/calpoly/razsoftware/CourseTableModel.java
+++ src/edu/calpoly/razsoftware/CourseTableModel.java
@@ -7,109 +7,111 @@ import javax.swing.table.AbstractTableModel;
 import com.google.common.collect.ImmutableList;
 
 /**
- * An table model that accesses a backend model to display the values in the cells
- * It also delegates the changing of the data to a seperate controller class
+ * An table model that accesses a backend model to display the values in the
+ * cells It also delegates the changing of the data to a seperate controller
+ * class
+ * 
  * @author Daniel
- *
+ * 
  */
 class CourseTableModel extends AbstractTableModel
 {
-   private List<Course> sortedCatalog;
-   private CourseList   state;
-   SchedulerController  controller;
+    private List<Course> sortedCatalog;
+    private CourseList   state;
+    private SchedulerController  controller;
 
-   public CourseTableModel(CourseList state, SchedulerController controller,CourseList catalog)
-   {
-      this.controller = controller;
-      sortedCatalog =
-            ImmutableList.copyOf(catalog.getCourses());
-      this.state = state;
-   }
+    public CourseTableModel(CourseList state, SchedulerController controller,
+            CourseList catalog)
+    {
+        this.controller = controller;
+        sortedCatalog = ImmutableList.copyOf(catalog.getCourses());
+        this.state = state;
+    }
 
-   @Override
-   public int getRowCount()
-   {
-      return sortedCatalog.size();
-   }
+    @Override
+    public int getRowCount()
+    {
+        return sortedCatalog.size();
+    }
 
-   @Override
-   public int getColumnCount()
-   {
-      return 2;
-   }
+    @Override
+    public int getColumnCount()
+    {
+        return 2;
+    }
 
-   @Override
-   public String getColumnName(int columnIndex)
-   {
-      switch (columnIndex)
-      {
-         case 0:
-            return "Passed";
-         case 1:
-            return "Course";
-         default:
+    @Override
+    public String getColumnName(int columnIndex)
+    {
+        switch (columnIndex)
+        {
+            case 0:
+                return "Passed";
+            case 1:
+                return "Course";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex)
+    {
+        switch (columnIndex)
+        {
+            case 0:
+                return Boolean.class;
+            case 1:
+                return Course.class;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex)
+    {
+        switch (columnIndex)
+        {
+            case 0:
+                return true;
+            case 1:
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex)
+    {
+        if (rowIndex > sortedCatalog.size())
             return null;
-      }
-   }
 
-   @Override
-   public Class<?> getColumnClass(int columnIndex)
-   {
-      switch (columnIndex)
-      {
-         case 0:
-            return Boolean.class;
-         case 1:
-            return Course.class;
-         default:
-            return null;
-      }
-   }
+        final Course requestedCourse = (Course) sortedCatalog.get(rowIndex);
 
-   @Override
-   public boolean isCellEditable(int rowIndex, int columnIndex)
-   {
-      switch (columnIndex)
-      {
-         case 0:
-            return true;
-         case 1:
-            return false;
-         default:
-            return false;
-      }
-   }
+        switch (columnIndex)
+        {
+            case 0:
+                return state.contains(requestedCourse);
+            case 1:
+                return requestedCourse;
+            default:
+                return null;
+        }
+    }
 
-   @Override
-   public Object getValueAt(int rowIndex, int columnIndex)
-   {
-      if (rowIndex > sortedCatalog.size())
-         return null;
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex)
+    {
+        if (rowIndex > sortedCatalog.size())
+            return;
 
-      final Course requestedCourse = (Course) sortedCatalog.get(rowIndex);
-
-      switch (columnIndex)
-      {
-         case 0:
-            return state.contains(requestedCourse);
-         case 1:
-            return requestedCourse;
-         default:
-            return null;
-      }
-   }
-
-   @Override
-   public void setValueAt(Object aValue, int rowIndex, int columnIndex)
-   {
-      if (rowIndex > sortedCatalog.size())
-         return;
-
-      switch (columnIndex)
-      {
-         case 0:
-            controller.CheckBoxClicked();
-            break;
-      }
-   }
+        switch (columnIndex)
+        {
+            case 0:
+                controller.checkBoxClicked();
+                break;
+        }
+    }
 }
--- src/edu/calpoly/razsoftware/Main.java
+++ src/edu/calpoly/razsoftware/Main.java
@@ -6,65 +6,64 @@
 package edu.calpoly.razsoftware;
 
 import javax.swing.UIManager;
-import javax.swing.event.ListDataListener;
 
 /**
- * 
+ * The main methos the initilaizes different componets and links them together
  * @author light
  */
 public class Main
 {
 
-   /**
-    * @param args
-    *           the command line arguments
-    */
-   public static void main(String[] args)
-   {
-      try
-      {
-         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      }
-      catch (ClassNotFoundException ex)
-      {
-         java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
-               .log(java.util.logging.Level.SEVERE, null, ex);
-      }
-      catch (InstantiationException ex)
-      {
-         java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
-               .log(java.util.logging.Level.SEVERE, null, ex);
-      }
-      catch (IllegalAccessException ex)
-      {
-         java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
-               .log(java.util.logging.Level.SEVERE, null, ex);
-      }
-      catch (javax.swing.UnsupportedLookAndFeelException ex)
-      {
-         java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
-               .log(java.util.logging.Level.SEVERE, null, ex);
-      }
+    /**
+     * Starts the running of the program
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException ex)
+        {
+            java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (InstantiationException ex)
+        {
+            java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (IllegalAccessException ex)
+        {
+            java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        catch (javax.swing.UnsupportedLookAndFeelException ex)
+        {
+            java.util.logging.Logger.getLogger(SchedulerFrame.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
-      CourseList coursesTaken = new CourseList();
-      CourseList coursesRequired = new CourseList();
+        CourseList coursesTaken = new CourseList();
+        CourseList coursesRequired = new CourseList();
 
-      CourseList suggestedSchedule = new CourseList();
+        CourseList suggestedSchedule = new CourseList();
 
-      SchedulerView view = new SchedulerView();
+        SchedulerView view = new SchedulerView();
 
-      SchedulerController controller =
-            new SchedulerController(coursesTaken, coursesRequired,
-                  suggestedSchedule, view);
+        SchedulerController controller =
+                new SchedulerController(coursesTaken, coursesRequired,
+                        suggestedSchedule, view);
 
-      CourseTableModel m =
-            new CourseTableModel(coursesTaken, controller,
-                  controller.getCatalog());
-      view.setSources(m, coursesRequired,suggestedSchedule);
-      
+        CourseTableModel tableModel =
+                new CourseTableModel(coursesTaken, controller,
+                        controller.getCatalog());
+        view.setSources(tableModel, coursesRequired, suggestedSchedule);
 
-      view.setController(controller);
-      view.setVisible(true);
+        view.setController(controller);
+        view.setVisible(true);
 
-   }
+    }
 }
