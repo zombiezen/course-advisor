f6d9229fd4fd97d4a5bf1a88d7a778089938baaa
aspurgin <aspurgin@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-11-18 05:38:39 +0000

    refactoring, cleaning

--- src/edu/calpoly/razsoftware/CatConvCourse.java
+++ src/edu/calpoly/razsoftware/CatConvCourse.java
@@ -18,79 +18,80 @@ import com.google.gson.Gson;
  */
 public class CatConvCourse 
 {
-   public List<String> major;
-   public int number, units;
-   public String description;
-   public String name = "";
-   public Set<Set<CatConvCourse>> preRequisites;//list of lists for possiblities
+    private List<String> major;
+    private int number;
+    private int units;
+    private String description;//list of lists for possiblities
                                                 //outside or, inside and
-   public Set<Set<CatConvCourse>> coRequisites;// pre or co ^^
+    private String name = "";// pre or co ^^
+    private Set<Set<CatConvCourse>> preRequisites;
+    private Set<Set<CatConvCourse>> coRequisites;
    
    /**
     * constructs a default course, initializes all values to default
     */
-   public CatConvCourse()
-   {
+    public CatConvCourse()
+    {
        major = new ArrayList<String>();
        number = 0;
        units = 0;
        description = new String("");
        preRequisites = new HashSet<Set<CatConvCourse>>();
        coRequisites = new HashSet<Set<CatConvCourse>>();
-   }
+    }
    
    /**
     * as in the default sense of the method
     * @param obj object to be compared to
     * @return if the object is similarly equivalent
     */
-   public boolean equals(Object obj)
-   {
+    public boolean equals(Object obj)
+    {
        //IF the class of this course does not equal the class of the object
-       if(this.getClass() != obj.getClass())
-           return false;
-       //SET eq to false
-       boolean eq = false;
-       //FOR every string in the major field
-       for(String s : major)                    //has common major name
-       {
-           //FOR every string in the comparing object's major field
-           for(String s2 : ((CatConvCourse)obj).major)
-           {
-               //SET eq as true if they match
-               eq |= (s.equalsIgnoreCase(s2));
-           }
-           //ENDFOR
-       }
-       //ENDFOR
-       //SET eq as false if the numbers don't match
-       eq &= (number == ((CatConvCourse)obj).number);    
-       //RETURN eq
-       return eq;       
-   }
-   
-   /**
-    * constructs a course with a given major and number
-    * @param major string of major code
-    * @param number value of course number
-    */
-   public CatConvCourse(String major, int number)
-   {
-       this.major = new ArrayList<String>();
-       this.major.add(major);
-       this.number = number;
-   }
-   
-   /*---------'get' methods---------*/
-   public int getNumber()
-   {
-       return number;
-   }
-   
-   public String getName()
-   {
-       return name;
-   }
+        if(this.getClass() != obj.getClass())
+            return false;
+        //SET eq to false
+        boolean eq = false;
+        //FOR every string in the major field
+        for(String s : getMajor())                    //has common major name
+        {
+            //FOR every string in the comparing object's major field
+            for(String s2 : ((CatConvCourse)obj).getMajor())
+            {
+                //SET eq as true if they match
+                eq |= (s.equalsIgnoreCase(s2));
+            }
+            //ENDFOR
+        }
+        //ENDFOR
+        //SET eq as false if the numbers don't match
+        eq &= (getNumber() == ((CatConvCourse)obj).getNumber());    
+        //RETURN eq
+        return eq;       
+    }
+    
+    /**
+     * constructs a course with a given major and number
+     * @param major string of major code
+     * @param number value of course number
+     */
+    public CatConvCourse(String major, int number)
+    {
+        this.major = new ArrayList<String>();
+        this.major.add(major);
+        this.number = number;
+    }
+    
+    /*---------'get' methods---------*/
+    public int getNumber()
+    {
+        return number;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
    public int getUnits()
    {
        return units;
@@ -98,7 +99,7 @@ public class CatConvCourse
    
    public String getDesc()
    {
-       return description;
+       return getDescription();
    }
     
    /*---------'set' methods---------*/
@@ -114,7 +115,7 @@ public class CatConvCourse
    
    public void setDesc(String Description)
    {
-       description = Description;
+        setDescription(Description);
    }
    
    public void setName(String Name)
@@ -124,7 +125,71 @@ public class CatConvCourse
    
    public void setMajor(Collection<String> Major)
    {
-       major = new ArrayList<String>(Major);
+        setMajor(new ArrayList<String>(Major));
    }
+
+    /**
+     * @return the major
+     */
+    public List<String> getMajor()
+    {
+        return major;
+    }
+
+    /**
+     * @param major the major to set
+     */
+    public void setMajor(List<String> major)
+    {
+        this.major = major;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    /**
+     * @return the preRequisites
+     */
+    public Set<Set<CatConvCourse>> getPreRequisites()
+    {
+        return preRequisites;
+    }
+
+    /**
+     * @param preRequisites the preRequisites to set
+     */
+    public void setPreRequisites(Set<Set<CatConvCourse>> preRequisites)
+    {
+        this.preRequisites = preRequisites;
+    }
+
+    /**
+     * @return the coRequisites
+     */
+    public Set<Set<CatConvCourse>> getCoRequisites()
+    {
+        return coRequisites;
+    }
+
+    /**
+     * @param coRequisites the coRequisites to set
+     */
+    public void setCoRequisites(Set<Set<CatConvCourse>> coRequisites)
+    {
+        this.coRequisites = coRequisites;
+    }
    
 }
