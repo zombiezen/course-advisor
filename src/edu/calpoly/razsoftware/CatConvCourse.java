df625f7632eeb92f2789a307c4c0d6b22416aeb9
aspurgin <aspurgin@9b0f9b5d-5442-4158-a236-97042d13d926>
2011-11-18 02:53:52 +0000

    Pseudocode

--- src/edu/calpoly/razsoftware/CatConvCourse.java
+++ src/edu/calpoly/razsoftware/CatConvCourse.java
@@ -1,176 +1,130 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.calpoly.razsoftware;
 
-/**
- *
- * @author adam
- */
+
 import java.util.*;
 
 import com.google.gson.Gson;
-
-public class CatConvCourse
+/**
+ * this class represents a course. a general description for a quarter-long
+ * session of instruction. independent of time period offered or specific
+ * section. this was put together outside the production of the official course
+ * object for swiftness of development and getting the course data.
+ * 
+ * DO NOT use this object outside of purely developer scope
+ * DO NOT use this in any method or class the user directly or indirectly
+ * accesses
+ * 
+ * @author adam
+ */
+public class CatConvCourse 
 {
-    List<String>            major;
-    int                     number, units;
-    String                  description;
-    String                  name = "";
-    Set<Set<CatConvCourse>> preRequisites; // list of lists for
-                                           // possiblities, outside or,
-                                           // inside and
-    Set<Set<CatConvCourse>> coRequisites; // pre or co ^^
-
-    public CatConvCourse()
-    {
-        major = new ArrayList<String>();
-        number = 0;
-        units = 0;
-        description = new String("");
-        preRequisites = new HashSet<Set<CatConvCourse>>();
-        coRequisites = new HashSet<Set<CatConvCourse>>();
-    }
-
-    public boolean equals(Object obj)
-    {
-        if (this.getClass() != obj.getClass()) // is same class
-            return false;
-        boolean eq = false;
-        for (String s : major) // has common major name
-        {
-            for (String s2 : ((CatConvCourse) obj).major)
-            {
-                eq |= (s.equalsIgnoreCase(s2));
-            }
-        }
-        eq &= (number == ((CatConvCourse) obj).number); // has same course
-                                                        // number
-        return eq;
-    }
-
-    public CatConvCourse(String major, int number)
-    {
-        this.major = new ArrayList<String>();
-        this.major.add(major);
-        this.number = number;
-    }
-
-    public String ToBasicString()
-    {
-        StringBuilder sb = new StringBuilder("");
-        for (String s : major)
-        {
-            sb.append(s + "/");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append(" " + Integer.toString(number));
-        return sb.toString();
-    }
-
-    public String ToString()
-    {
-        StringBuilder sb = new StringBuilder("");
-        for (String s : major)
-        {
-            sb.append(s + "/");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append(" \t ").append(Integer.toString(number));
-        sb.append(" \t " + Integer.toString(units));
-        sb.append(" \t " + name);
-        sb.append(" \t \"" + description + "\"");
-        sb.append(" \t, [");
-        if (preRequisites != null && preRequisites.size() != 0)
-        {
-            for (Set<CatConvCourse> s : preRequisites)
-            {
-                CatConvCourse[] CatConvCourses = new CatConvCourse[s.size()];
-                s.toArray(CatConvCourses);
-                sb.append("[");
-                for (CatConvCourse c : CatConvCourses)
-                {
-                    sb.append(c.ToBasicString() + " ");
-                }
-                sb.append("]");
-            }
-        }
-        sb.append("]");
-        sb.append(" \t [");
-        if (coRequisites.size() != 0)
-        {
-            for (Set<CatConvCourse> s : coRequisites)
-            {
-                CatConvCourse[] CatConvCourses = new CatConvCourse[s.size()];
-                s.toArray(CatConvCourses);
-                sb.append("[");
-                for (CatConvCourse c : CatConvCourses)
-                {
-                    sb.append(c.ToBasicString()).append(" ");
-                }
-                sb.append("]");
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
-    /*---------'get' methods---------*/
-    public int getNumber()
-    {
-        return number;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public int getUnits()
-    {
-        return units;
-    }
-
-    public String getDesc()
-    {
-        return description;
-    }
-
-    /*---------'set' methods---------*/
-    public void setNumber(int Number)
-    {
-        number = Number;
-    }
-
-    public void setUnits(int Units)
-    {
-        units = Units;
-    }
-
-    public void setDesc(String Description)
-    {
-        description = Description;
-    }
-
-    public void setName(String Name)
-    {
-        name = Name;
-    }
-
-    public void setMajor(Collection<String> Major)
-    {
-        major = new ArrayList<String>(Major);
-    }
-
-    public CatConvCourse(List<String> major, int number, int units,
-            String description)
-    {
-
-    }
-
-    boolean preRecsMet(List<CatConvCourse> taken)
-    {
-        return false;
-    }
+   public List<String> major;
+   public int number, units;
+   public String description;
+   public String name = "";
+   public Set<Set<CatConvCourse>> preRequisites;//list of lists for possiblities
+                                                //outside or, inside and
+   public Set<Set<CatConvCourse>> coRequisites;// pre or co ^^
+   
+   /**
+    * constructs a default course, initializes all values to default
+    */
+   public CatConvCourse()
+   {
+       major = new ArrayList<String>();
+       number = 0;
+       units = 0;
+       description = new String("");
+       preRequisites = new HashSet<Set<CatConvCourse>>();
+       coRequisites = new HashSet<Set<CatConvCourse>>();
+   }
+   
+   /**
+    * as in the default sense of the method
+    * @param obj object to be compared to
+    * @return if the object is similarly equivalent
+    */
+   public boolean equals(Object obj)
+   {
+       //IF the class of this course does not equal the class of the object
+       if(this.getClass() != obj.getClass())
+           return false;
+       //SET eq to false
+       boolean eq = false;
+       //FOR every string in the major field
+       for(String s : major)                    //has common major name
+       {
+           //FOR every string in the comparing object's major field
+           for(String s2 : ((CatConvCourse)obj).major)
+           {
+               //SET eq as true if they match
+               eq |= (s.equalsIgnoreCase(s2));
+           }
+           //ENDFOR
+       }
+       //ENDFOR
+       //SET eq as false if the numbers don't match
+       eq &= (number == ((CatConvCourse)obj).number);    
+       //RETURN eq
+       return eq;       
+   }
+   
+   /**
+    * constructs a course with a given major and number
+    * @param major string of major code
+    * @param number value of course number
+    */
+   public CatConvCourse(String major, int number)
+   {
+       this.major = new ArrayList<String>();
+       this.major.add(major);
+       this.number = number;
+   }
+   
+   /*---------'get' methods---------*/
+   public int getNumber()
+   {
+       return number;
+   }
+   
+   public String getName()
+   {
+       return name;
+   }
+   public int getUnits()
+   {
+       return units;
+   }
+   
+   public String getDesc()
+   {
+       return description;
+   }
+    
+   /*---------'set' methods---------*/
+   public void setNumber(int Number)
+   {
+       number = Number;
+   }
+   
+   public void setUnits(int Units)
+   {
+       units = Units;
+   }
+   
+   public void setDesc(String Description)
+   {
+       description = Description;
+   }
+   
+   public void setName(String Name)
+   {
+       name = Name;
+   }
+   
+   public void setMajor(Collection<String> Major)
+   {
+       major = new ArrayList<String>(Major);
+   }
+   
 }
