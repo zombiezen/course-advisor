7769b0fa9e4577dafefb28b81d9947604426f18a
Ross Light <ross@zombiezen.com>
2011-12-06 19:54:45 +0000

    Add Apache License 2.0 to project

--- CourseAdvisor/src/edu/calpoly/razsoftware/SchedulerView.java
+++ CourseAdvisor/src/edu/calpoly/razsoftware/SchedulerView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 RAZ Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.calpoly.razsoftware;
 
 import java.awt.Component;
@@ -47,7 +63,7 @@ public class SchedulerView extends JFrame implements Observer
     /**
      * The current version number to display in the about box
      */
-    private static final String              kVersion = "v1.0";
+    private static final String              kVersion = "v1.3";
 
     /**
      * The name of the authors so they can be shown as contributers in the about
@@ -65,7 +81,9 @@ public class SchedulerView extends JFrame implements Observer
      * about box.
      */
     private static final String              kLicense =
-                                                              "\nGson: Copyright \u00a9 "
+                                                              "\nCourseAdvisor: Copyright \u00a9 "
+                                                                      + "2011 RAZ Software.\n"
+                                                                      + "Gson: Copyright \u00a9 "
                                                                       + "2008-2011 Google Inc.\n"
                                                                       + "Guava: Copyright \u00a9 "
                                                                       + "2010-2011 Google Inc.\n"
